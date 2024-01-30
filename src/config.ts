4a445b1e239d763af5832b10ebcf77b692fd38e3
shaochun <shaochun@users.noreply.github.com>
2024-01-30 15:19:29 +0800

    fix pagination error

--- src/config.ts
+++ src/config.ts
@@ -181,6 +181,7 @@ export class Config {
 
     if (config.pagination) {
       // Pagination
+      config.plugin.remove('pagination');
       config.plugin.add({
         id: 'pagination',
         position: PluginPosition.Footer,
@@ -191,6 +192,7 @@ export class Config {
     // Additional plugins
     if (config.plugins) {
       config.plugins.forEach((p) => config.plugin.add(p));
+      config.plugins.forEach((p) => {config.plugin.remove(p.id);config.plugin.add(p);});
     }
 
     return config;
