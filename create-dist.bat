512926bf3786938768ef7874e84e6bfcae67b21d
shaochun <shaochun@users.noreply.github.com>
2024-07-16 17:46:37 +0800

    create-dist tool: update heap size when building

--- create-dist.bat
+++ create-dist.bat
@@ -3,4 +3,5 @@
 rem #1 first do: pnpm i [this will wait for a while]
 
 rem #2 then can build using npm scripts
-pnpm run build:grid
+set NODE_OPTIONS="--max-old-space-size=8192"
+call pnpm run build:grid
